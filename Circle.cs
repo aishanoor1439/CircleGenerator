8146cdbfee7c8a716d02d1ceb5792069a24465bf
aishanoor1439 <aisha.noor1439@gmail.com>
2025-03-17 18:18:39 +0100

    Second Commit
    completed

---
+++ Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleGenerator
+{
+    class Circle
+    {
+
+        //Attribute
+        double radius;
+
+        //Methods 
+        public Circle()
+        {
+            this.radius = 0;
+        }
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public string getCircle()
+        {
+            return string.Format("Circle : {0}", radius);
+        }
+
+        //Operator overloading
+        public static Circle operator +(Circle c1, Circle c2)
+        {
+            Circle c3 = new Circle();
+            c3.radius = c1.radius + c2.radius;
+            return c3;
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ namespace CircleGenerator
     {
         static void Main(string[] args)
         {
+            Circle c1 = new Circle(2.8);
+            Circle c2 = new Circle(4.5);
+            Circle c3 = c1 + c2;
+            Console.WriteLine(c1.getCircle());
+            Console.WriteLine(c2.getCircle());
+            Console.WriteLine(c3.getCircle());
+            Console.ReadKey();
         }
     }
 }
